74f0af19b9790885ba357e6f6f93fcf7af2a4b2f
mshattori <mshattori@gmail.com>
2024-01-08 13:56:18 +0900

    fix: fix to use 'opacity' instead of visibility.
    Using 'element.style.visibility' isn't avairable because hidden elements
    become unclickable from the screen thus you can't toggle them.
    Therefore fixed to use 'element.style.opacity' instead.

--- www/toggle-answers.js
+++ www/toggle-answers.js
@@ -1,12 +1,20 @@
+let globalVisibility = 1
+
 document.addEventListener('DOMContentLoaded', function() {
+    // Set click event listener to toggle visibility
+    document.querySelectorAll('.answer-text').forEach(function(element) {
+        // NOTE: Using 'element.style.visibility' isn't avairable because hidden elements
+        // become unclickable from the screen thus you can't toggle them.
+        element.style.opacity = globalVisibility;
+        element.addEventListener('click', function() {
+            element.style.opacity = element.style.opacity == 1 ? 0 : 1;
+        });
+    });
     document.getElementById('toggle-answers').addEventListener('click', function() {
-        // Toggle visibility
+        // Toggle visibility of all target element
+        globalVisibility = globalVisibility == 1 ? 0 : 1;
         document.querySelectorAll('.answer-text').forEach(function(element) {
-            if (element.style.visibility === 'hidden') {
-                element.style.visibility = 'visible';
-            } else {
-                element.style.visibility = 'hidden';
-            }
+            element.style.opacity = globalVisibility;
         });
     });
 });
